9155f8ff928d3b96b2611ac6e059c553848b5cc8
cfy <1002161225@qq.com>
2022-05-17 15:43:18 +0800

    补充代码注释

--- src/Migrate/MinePhinxTable.php
+++ src/Migrate/MinePhinxTable.php
@@ -8,6 +8,9 @@ use think\migration\db\Table;
 
 class MinePhinxTable extends Table {
 
+    /**
+     * @return Table
+     */
     public function addFingerPrint(): Table {
         $this->addColumn('create_time', 'timestamp', ['comment' => '数据创建时间', 'default' => 'CURRENT_TIMESTAMP'])
              ->addColumn('create_by', 'integer', ['comment' => '创建人', 'signed' => false])
@@ -23,12 +26,26 @@ class MinePhinxTable extends Table {
         return $this;
     }
 
+    /**
+     * @param \Phinx\Db\Table\Column|string $columnName
+     * @param null $type
+     * @param array $options
+     *
+     * @return $this
+     */
     public function addColumn($columnName, $type = null, $options = [])
     {
         parent::addColumn($columnName, $type, $options);
         return $this;
     }
 
+    /**
+     * @param string $columnName
+     * @param null $newColumnType
+     * @param array $options
+     *
+     * @return $this
+     */
     public function changeColumn($columnName, $newColumnType = null, $options = []) {
         parent::changeColumn($columnName, $newColumnType, $options);
         return $this;
